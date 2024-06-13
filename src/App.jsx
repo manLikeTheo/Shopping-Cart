03dc382caca7ff5b963b64e1591a508d67b5e386
unknown <adewaletheophilus@gmail.com>
2024-06-13 13:16:15 +0100

    Added tailwindcss and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Shopping Cart Project</h1>
+      <Navbar />
+      <h1 className="text-2xl text-slate-500 font-bold font-green">
+        Shopping Cart Project
+      </h1>
     </>
   );
 }
---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <div className="logo-text">
+        <h1 className="text-3xl text-amber-400 font-semibold">
+          Sauce!<span className="text-4xl font-bold text-zinc-500">Wears</span>
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default Navbar;
