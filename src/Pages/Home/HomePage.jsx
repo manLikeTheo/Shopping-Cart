381971015c44319461c28eb01c535bc5f9f1477a
theophilus adewale <adewaletheophilus@gmail.com>
2024-10-13 05:12:13 +0100

    Add Footer and Image credits

--- src/Pages/Home/HomePage.jsx
+++ src/Pages/Home/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import backgroundImage from "../../assets/nuno-alberto-bg.jpg";
+import { FaGithub } from "react-icons/fa";
+import { FaImage } from "react-icons/fa";
 const HomePage = () => {
   return (
     <div
-      className="h-screen bg-cover bg-center flex flex-col justify-center items-center"
+      className="h-screen bg-cover bg-center flex flex-col justify-center items-center relative"
       style={{ backgroundImage: `url(${backgroundImage})` }}
     >
       <div className="intro w-50 opacity-90 bg-transparent p-6 text-center flex flex-col justify-center items-center">
@@ -21,11 +23,30 @@ const HomePage = () => {
           too good? Check out our products!
         </p>
         <Link to="/shop">
-          <button className="text-2xl text-white font-bold bg-amber-700 p-3 rounded-md m-4 hover: scale-95 hover:opacity-95">
+          <button className="text-2xl text-white font-bold bg-amber-700 p-3 rounded-md m-4 hover:scale-95 hover:opacity-90">
             Go To Shop
           </button>
         </Link>
       </div>
+      <footer className=" mb-2 absolute bottom-0 flex gap-8 justify-center items-center">
+        <p className="text-amber-500 text-lg">© 2024</p>
+        <Link
+          to="https://github.com/manLikeTheo/Shopping-Cart"
+          className="text-amber-600 hover:scale-90"
+        >
+          {<FaGithub size={28} />}
+        </Link>
+        <p className="text-amber-500 text-lg flex gap-4 justify-center items-center">
+          Image credits{" "}
+          <Link
+            to="https://unsplash.com/photos/man-in-front-of-plush-toy-claw-machine-XOJoyZYTezo?like_photo=true"
+            className="text-blue-700"
+          >
+            {<FaImage size={24} className="hover:scale-105" />}
+          </Link>
+          : Nuno Alberto on Unsplash
+        </p>
+      </footer>
     </div>
   );
 };
